319d3b05bbfa2bc1d0afab43e5ec7a039665b93e
rajarajan <rajarajan.rajendran@luminad.com>
2019-06-03 15:08:43 +0530

    First Commit

---
+++ assets/js/data.js
@@ -0,0 +1,25 @@
+var direction_text='<span id="dir_span">Directions:</span> Click on each question mark to progress through the graphic. You may close the labels by clicking on the “X.”'
+var slider=[
+	{	slide_text:'<b>(a)</b> El flan, postre típico de los países hispanos. Dependiendo del país, varían los ingredientes. Por ejemplo, en el Caribe a veces se añade coco o piña.',
+		slide_img:'img_1',
+	 	img_alt_text:"In this photograph, a rectangular slice of yellow, dense flan sits on a plate with flowers on it. Courtesy Conchita Lucas"
+	},
+	{	slide_text:'<b>(b) Recipientes necesarios:</b> flanera y fuente para el baño maría.',
+		slide_img:'img_2',
+	 	img_alt_text:'In this photograph, there is an image of two pans on a counter. One of the pans is long and has low sides. A line with the text fuente para el el baño María, double boiler, points to this pan. The other pan, which is made of metal and is much shorter but deeper than the other pan, has a line with the word flanera pointing to it. Courtesy Conchita Lucas.'
+	},
+	{	slide_text:'<b>(c) El caramelo</b> es un almíbar que se forma cuando se calienta el agua con el azúcar.',
+		slide_img:'img_3',
+	 	img_alt_text:'In this photograph, the flanera has bubbling, brown el caramelo in the bottom of it. Courtesy Conchita Lucas'
+	},
+	{	slide_text:'<b>(d) El baño</b> <span><div class="clickable_1" id="click_popup" style="display: inline-block" aria-label="María 7"> María<sup>7</sup></div> <div aria-label="7 double-boiler. To move to the close button press the tab key." id="pop_up"><div id="box_arrow"></div><sup>7</sup>double-boiler<div id="close_btn" aria-label="To close the glossary popup window, press the Enter or Space bar key."></div></div></span> consiste en poner el molde con el flan dentro de otro recipiente que contiene agua.',
+		slide_img:'img_4',
+	 	img_alt_text:'In this photograph, the flanera with the flan inside is inside of the longer, more shallow pan. There is water in the more shallow pan. Courtesy Conchita Lucas'
+	},
+	{	slide_text:'<b>(e)</b> Cuando el flan está frío, se separa sin dificultad de los bordes del molde con un cuchillo y se vuelca en el plato de servir.',
+		slide_img:'img_5',
+	 	img_alt_text:'In this photograph, the finished flan has been placed on a dish. Courtesy Conchita Lucas'
+	}
+],
+
+thumb_label=['To select full size image of this thumbnail and corresponding details, press the enter or spacebar key. To move to next thumbnail, press the tab key.']
---
+++ assets/js/script.js
@@ -0,0 +1,277 @@
+var begin_entered=false;
+var direction_text='<span id="dir_span">Directions:</span> Click on each question mark to progress through the graphic. You may close the labels by clicking on the “X.”'
+$(document).ready(function(){
+	if (/MSIE 10/i.test(navigator.userAgent) || /MSIE 9/i.test(navigator.userAgent) || /rv:11.0/i.test(navigator.userAgent) ||/Edge\/\d./i.test(navigator.userAgent)) {
+		//$('.dummy_popup,.hotspot_dummy,#dummy_1,#dummy_2').attr('aria-hidden','true').remove()
+		//$('.dummy_popup,.hotspot_dummy').removeAttr('tabindex').remove()
+	}
+	
+	if( /Android|webOS|iPhone|iPad|iPod|Opera Mini/i.test(navigator.userAgent) ) {
+		$('#label_cont_2,#label_cont_4').attr('role','text');
+	}
+	
+	$('#text_container').html(direction_text)
+	$('#begin_btn').off('click').on('click',function(){
+			//$('.label_content').attr('role','application');
+		$('a').attr('href','#text_container');
+			begin_entered=true
+		//setTimeout(function(){
+			$('#begin_page').hide()	
+			$('#text_container,#responsive_container').show();
+			resizeApp();
+			set_tab();
+			$('#reset_btn').off('click').on('click',pageReload);
+			if (/MSIE 10/i.test(navigator.userAgent) || /MSIE 9/i.test(navigator.userAgent) || /rv:11.0/i.test(navigator.userAgent) ||/Edge\/\d./i.test(navigator.userAgent)) {
+				$('#whole_container').removeAttr('role');
+			}else{
+				$('#whole_container').attr('role','application');
+			}
+			//$('#heading_text').attr('tabindex','0').focus().blur();
+		//},100)
+	})
+	
+	$("#begin_container,#image_container").mouseenter(function(){
+		$('#begin_container,#image_container').attr('title',"Capturing energy from the environment");
+	});
+
+	$("#begin_container,#image_container").mouseleave(function() {
+		$(this).removeAttr('title');
+	});
+	
+	$('.hotspot').off('click').on('click',function(e){
+		//setTimeout(function(){
+			var curr_id=e.target.id.split('_')[1];
+			$('#toggle_'+curr_id).hide();
+			$('.hotspot_dummy').hide();
+//			$('.label_class').hide();
+			//$('.hotspot').show();
+//			$('#toggle_'+curr_id).hide();
+			
+//			$('#hotspot_dummy_'+curr_id).show();
+//			set_tab();
+//			console.log(curr_id)
+//			$('#toggle_3').removeAttr('role');
+			$('.dummy_popup').attr('aria-hidden','true').hide()
+//			$('#'+e.target.id).hide();
+//			$('#heading_text').css('visibility','hidden');
+//			$('.label_class').hide();
+			$('#label_'+curr_id).show();
+			if (/MSIE 10/i.test(navigator.userAgent) || /MSIE 9/i.test(navigator.userAgent) || /rv:11.0/i.test(navigator.userAgent) ||/Edge\/\d./i.test(navigator.userAgent)) {
+				setTimeout(function(){
+					$('.label_content').attr('tabindex','0').focus()//.css('outline','1px solid');
+				},10);
+			}
+//		setTimeout(function(){
+//			$('.tab_index').focus().blur();
+//		},10);
+		//},100)
+		//$('.label_content').attr('role','application');
+	});
+	$('.close_button').off('click').on('click',function(e){
+		var curr_id=e.target.id.split('_')[1];
+		$('#label_'+curr_id).hide()//.remove();
+//		if (/MSIE 10/i.test(navigator.userAgent) || /MSIE 9/i.test(navigator.userAgent) || /rv:11.0/i.test(navigator.userAgent) ||/Edge\/\d./i.test(navigator.userAgent)) {
+//			//$('.dummy_popup,.hotspot_dummy,#dummy_1,#dummy_2').attr('aria-hidden','true').remove()
+//			//$('.dummy_popup,.hotspot_dummy').removeAttr('tabindex').remove()
+//		}else{
+			$('.hotspot_dummy').attr('aria-hidden','false')
+//		}
+		
+		
+		//setTimeout(function(){
+//			$('.hotspot').show();	
+			$('#toggle_'+curr_id).show()
+			$('#heading_text').css('visibility','visible');	
+		
+		$('#toggle_3').attr('aria-label','clickable hotspot')
+		$('#toggle_3').attr('aria-label','Hotspot 5 of 7')//.focus();
+//			alert();
+		//},10)
+		if (/MSIE 10/i.test(navigator.userAgent) || /MSIE 9/i.test(navigator.userAgent) || /rv:11.0/i.test(navigator.userAgent) ||/Edge\/\d./i.test(navigator.userAgent)) {
+			setTimeout(function(){
+				console.log(curr_id)
+				$('#toggle_'+curr_id).focus().attr('aria-hidden','true')//.css('outline','0px solid');
+			},10);
+		}
+	})
+	resizeApp();
+	set_tab();
+	
+		
+//		$('.tab_index').click(function(e){
+////			$(this).addClass('clicked');
+//			e.stopImmediatePropagation();
+//			
+//		});
+//		$('.tab_index').focus(function(){
+//			$(this).removeClass('clicked');
+//		});
+	
+	//..............................tab...............................//
+//	$('#reverse_extra_tab').on('focus',function(){
+//		//$('.label_content').removeAttr('role');
+//		$('.tab_index').eq(1).focus();
+//	});
+	//heading_text
+	
+	
+	
+	
+	$('.label_content').on('focus',function(){
+		$('#text_container').removeAttr('role');
+			$('.label_class').removeAttr('tabindex')//.focus();
+		//$('.label_content').removeAttr('role');
+	})
+	$('.hotspot').on('focus',function(){
+		//$('.label_content').attr('role','application');
+		if (/MSIE 10/i.test(navigator.userAgent) || /MSIE 9/i.test(navigator.userAgent) || /rv:11.0/i.test(navigator.userAgent) ||/Edge\/\d./i.test(navigator.userAgent)) {
+			setTimeout(function(){
+//				console.log(curr_id)
+				$('.hotspot').attr('aria-hidden','false')
+			},10);
+		}
+		
+	})
+	$('#begin_dummy').on('focus',function(){
+		$('.tab_index').eq(1).focus();	
+	})
+	$('#begin_reverse_dummy').on('focus',function(){
+		$('.tab_index').eq(1).focus();
+	})
+	$('.dummy_popup').on('focus',function(){
+		//alert();
+//		$('.label_content').eq(0).focus();
+		$('.dummy_popup').attr('aria-hidden','true')
+		setTimeout(function(){
+			$('.dummy_popup').attr('aria-hidden','true')
+		},10)
+	});
+	
+	$('#hotspot_dummy_1').on('focus',function(){
+//		alert()
+		setTimeout(function(){
+			$('#toggle_1').focus();
+			$('.hotspot_dummy').attr('aria-hidden','true')
+		},10)
+	})
+	
+	$('#hotspot_dummy_2').on('focus',function(){
+//		alert()
+		setTimeout(function(){
+			$('#toggle_2').focus();
+			$('.hotspot_dummy').attr('aria-hidden','true')
+		},10)
+	})
+
+		$('#hotspot_dummy_3').on('focus',function(){
+			setTimeout(function(){
+				$('#toggle_3').focus().attr('role','button')
+				$('#toggle_3').focus().attr('aria-label','hotspot 5 of 7')
+				
+				$('.hotspot_dummy,.dummy_popup').attr('aria-hidden','true')
+			},5)
+		})
+	
+	$('.label_content').on('focus',function(){
+		setTimeout(function(){
+			//$('.label_content').removeAttr('role');
+		},1000)
+//		$('.label_content').removeAttr('role');
+//		$('.label_content').removeAttr('role');
+		//setTimeout(function(){
+		//},100)
+	})
+	
+	
+	$('#dummy_1').on('focus',function(){
+		//alert();
+		//setTimeout(function(){
+			$('.tab_index').eq(1).focus();
+			$('#whole_container').removeAttr('role');
+		//},10)		
+	})
+	$('#dummy_2').on('focus',function(){
+		//setTimeout(function(){
+			$('.tab_index').eq(0).focus();
+		//},10)
+	})
+	
+	//$('.hotspot').attr('aria-label','clickable hotspot');
+	$('#text_container').attr('aria-label',$('#text_container').text());
+	$('#heading_text').attr('aria-label',$('#heading_text').text())
+	
+	$('.label_btn').off('click').on('click',function(e){
+		if(e.target.id=='show_all'){
+			$('.hotspot').trigger('click')
+			$('#show_all').hide();
+			$('#hide_all').show()//.focus()//.attr('aria-hidden','true');
+			setTimeout(function(){
+				$('#hide_all').focus()
+			},10)
+//			alert('1')
+//			$('.label_btn').focus();
+//			$('#reset_btn').removeAttr('tabindex').attr('tabindex','0')
+//			set_tab()
+		}else{
+			$('.close_button').trigger('click')
+			$('#hide_all').hide();
+			$('#show_all').show();
+			setTimeout(function(){
+				$('#show_all').focus()
+			},10)
+			
+			//.attr('aria-hidden','true');
+//			alert('2')
+//			$('.label_btn').focus();
+//			set_tab()
+		}
+	});
+	
+document.body.onkeyup = function(e){
+//		alert()
+		console.log(document.activeElement)
+		if(e.keyCode == 32 || e.keyCode == 13){
+			e.preventDefault(e);
+			if(e.target.id!='label_head_1'||e.target.id!='label_head_2'||e.target.id!='label_head_3'){
+//				alert();
+				$('#'+e.target.id).focus();
+				$('#'+e.target.id).trigger('click');
+			}
+			
+		}
+		
+	}
+
+});
+$(window).resize(function(){
+  resizeApp();
+});
+var pageReload = function(){
+	location.reload();
+}
+function set_tab(){
+	if(begin_entered==false){
+		$('.tab_index').removeClass('tab_index').removeAttr('tabindex');
+		$('#begin_container').addClass('tab_index');
+		$('#begin_btn').addClass('tab_index');
+		$('#begin_dummy,#begin_reverse_dummy').addClass('tab_index');
+	}else{
+		$('.tab_index').removeClass('tab_index').removeAttr('tabindex');
+		$('#text_container').addClass('tab_index');
+		$('#heading_text').addClass('tab_index');
+		$('.hotspot').addClass('tab_index');
+		$('.label_content').addClass('tab_index');
+		$('.label_content').addClass('tab_index');
+		$('.close_button').addClass('tab_index');
+		$('#reset_btn').addClass('tab_index');
+		$('#dummy_1,#dummy_2,.dummy_popup,.hotspot_dummy,.label_btn').addClass('tab_index');
+	}
+	$('.tab_index').each(function(){
+//		setTimeout(function(){
+    		$('.tab_index').attr('tabindex','0')//.focus().blur();
+//		},10)
+  });
+}
+var begin_entered=false;
+var direction_text='<span id="dir_span">Directions:</span> Click on each question mark to progress through the graphic. You may close the labels by clicking on the “X.”'
+
